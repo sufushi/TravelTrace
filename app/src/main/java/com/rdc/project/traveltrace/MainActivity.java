cf5137f02e7711be574d89f920864fce4ba0bf13
-4 <2646094630@qq.com>
2019-03-10 22:58:37 +0800

    接入下拉刷新上拉加载组件BGARefreshLayout

--- app/src/main/java/com/rdc/project/traveltrace/MainActivity.java
+++ app/src/main/java/com/rdc/project/traveltrace/MainActivity.java
@@ -1,8 +1,16 @@
 package com.rdc.project.traveltrace;
 
+import android.os.AsyncTask;
+
 import com.rdc.project.traveltrace.base.BaseSwipeBackActivity;
 
-public class MainActivity extends BaseSwipeBackActivity {
+import cn.bingoogolapple.refreshlayout.BGANormalRefreshViewHolder;
+import cn.bingoogolapple.refreshlayout.BGARefreshLayout;
+import cn.bingoogolapple.refreshlayout.BGARefreshViewHolder;
+
+public class MainActivity extends BaseSwipeBackActivity implements BGARefreshLayout.BGARefreshLayoutDelegate {
+
+    private BGARefreshLayout mRefreshLayout;
 
     @Override
     protected int getLayoutResID() {
@@ -21,11 +29,74 @@ public class MainActivity extends BaseSwipeBackActivity {
 
     @Override
     protected void initView() {
-
+        mRefreshLayout = (BGARefreshLayout) findViewById(R.id.refresh_layout);
+        mRefreshLayout.setDelegate(this);
+        BGARefreshViewHolder viewHolder = new BGANormalRefreshViewHolder(this, true);
+        viewHolder.setLoadingMoreText("加载更多...");
+        viewHolder.setLoadMoreBackgroundColorRes(R.color.colorPrimary);
+        viewHolder.setLoadMoreBackgroundDrawableRes(R.mipmap.ic_launcher_round);
+        viewHolder.setRefreshViewBackgroundColorRes(R.color.colorAccent);
+        viewHolder.setRefreshViewBackgroundDrawableRes(R.mipmap.ic_launcher);
+        mRefreshLayout.setRefreshViewHolder(viewHolder);
     }
 
     @Override
     protected void initListener() {
 
     }
+
+    @Override
+    public void onBGARefreshLayoutBeginRefreshing(BGARefreshLayout bgaRefreshLayout) {
+        RefreshTask task = new RefreshTask();
+        task.execute();
+    }
+
+    @Override
+    public boolean onBGARefreshLayoutBeginLoadingMore(BGARefreshLayout bgaRefreshLayout) {
+        LoadMoreTask task = new LoadMoreTask();
+        task.execute();
+        return true;
+    }
+
+    private class RefreshTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (mRefreshLayout != null) {
+                mRefreshLayout.endRefreshing();
+            }
+        }
+
+    }
+
+    private class LoadMoreTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (mRefreshLayout != null) {
+                mRefreshLayout.endLoadingMore();
+            }
+        }
+    }
+
 }
