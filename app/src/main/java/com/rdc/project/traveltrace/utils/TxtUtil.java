acb6c1a22e27fe3113b3642a395cb066453dd7a5
-4 <2646094630@qq.com>
2019-04-04 22:53:06 +0800

    提交svg解析工具和txt读取工具类

---
+++ app/src/main/java/com/rdc/project/traveltrace/utils/TxtUtil.java
@@ -0,0 +1,80 @@
+package com.rdc.project.traveltrace.utils;
+
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class TxtUtil {
+
+    public static String readRawTxt(Context context, int rawId) {
+        InputStream inputStream = context.getResources().openRawResource(rawId);
+        return getString(inputStream);
+    }
+
+    private static String getString(InputStream inputStream) {
+        InputStreamReader inputStreamReader;
+        inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public static String readAssetTxt(Context context, String fileName) {
+        try {
+            InputStream inputStream = context.getAssets().open(fileName);
+            String code = getCode(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, code));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private static String getCode(InputStream is) {
+        try {
+            BufferedInputStream bin = new BufferedInputStream(is);
+            int p;
+            p = (bin.read() << 8) + bin.read();
+            String code;
+            switch (p) {
+                case 0xefbb:
+                    code = "UTF-8";
+                    break;
+                case 0xfffe:
+                    code = "Unicode";
+                    break;
+                case 0xfeff:
+                    code = "UTF-16BE";
+                    break;
+                default:
+                    code = "GBK";
+            }
+            is.close();
+            return code;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
