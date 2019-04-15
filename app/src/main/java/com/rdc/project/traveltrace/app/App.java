03253a6a680420c3d73a53408557d068a414791f
-4 <2646094630@qq.com>
2019-04-16 01:59:51 +0800

    引入bmob、rxjava、okHttp、gson等依赖库

--- app/src/main/java/com/rdc/project/traveltrace/app/App.java
+++ app/src/main/java/com/rdc/project/traveltrace/app/App.java
@@ -14,6 +14,9 @@ import com.lzy.ninegrid.NineGridView;
 import com.rdc.project.traveltrace.utils.GlideNineGirdImageLoader;
 import com.rdc.project.traveltrace.view.toast.CommonToast;
 
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobConfig;
+
 public class App extends Application {
 
     @SuppressLint("StaticFieldLeak")
@@ -22,22 +25,46 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        NineGridView.setImageLoader(new GlideNineGirdImageLoader());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
             StrictMode.setVmPolicy(builder.build());
         }
+        initNineGridViewConfig();
+        initPlayerConfig();
+        initToastConfig();
+        initBmob();
+    }
+
+    private void initNineGridViewConfig() {
+        NineGridView.setImageLoader(new GlideNineGirdImageLoader());
+    }
+
+    private void initPlayerConfig() {
         PlayerConfig.setUseDefaultNetworkEventProducer(true);
 //        PlayerLibrary.init(this);
         ExoMediaPlayer.init(this);
         PlayerConfig.playRecord(true);
         PlayRecordManager.setRecordConfig(new PlayRecordManager.RecordConfig.Builder().setMaxRecordCount(100).build());
+    }
+
+    private void initToastConfig() {
         CommonToast.Config.getInstance()
                 .tintIcon(true)
                 .setTextSize(12)
                 .allowQueue(true)
                 .apply();
     }
+
+    private void initBmob() {
+        BmobConfig bmobConfig = new BmobConfig.Builder(this)
+                .setApplicationId("d7396c68d5b566a8ab88509b2baf89d5")
+                .setConnectTimeout(30)
+                .setUploadBlockSize(1024 * 1024)
+                .setFileExpiration(2500)
+                .build();
+        Bmob.initialize(bmobConfig);
+    }
+
 
     @Override
     protected void attachBaseContext(Context base) {
