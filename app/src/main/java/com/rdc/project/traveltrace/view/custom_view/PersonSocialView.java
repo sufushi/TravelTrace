bdc6b0502fd19d91fecdc9e380eab9e5a4049155
-4 <2646094630@qq.com>
2019-04-20 23:51:06 +0800

    个人中心UI-添加按钮ripple点击效果

--- app/src/main/java/com/rdc/project/traveltrace/view/custom_view/PersonSocialView.java
+++ app/src/main/java/com/rdc/project/traveltrace/view/custom_view/PersonSocialView.java
@@ -4,14 +4,21 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.rdc.project.traveltrace.R;
 import com.rdc.project.traveltrace.arch.view.IView;
 import com.rdc.project.traveltrace.utils.DensityUtil;
+import com.rdc.project.traveltrace.view.DrawableTextView;
 
-public class PersonSocialView extends LinearLayout implements IView {
+public class PersonSocialView extends LinearLayout implements IView, View.OnClickListener {
+
+    private DrawableTextView mSocialGroupBtn;
+    private DrawableTextView mSocialFocusBtn;
+    private DrawableTextView mSocialCollectionBtn;
+    private DrawableTextView mSocialMessageBtn;
 
     public PersonSocialView(Context context) {
         this(context, null);
@@ -28,6 +35,20 @@ public class PersonSocialView extends LinearLayout implements IView {
         setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         int padding = DensityUtil.dp2px(10, context);
         setPadding(padding, padding, padding, padding);
+
+        initViews();
+    }
+
+    private void initViews() {
+        mSocialGroupBtn = findViewById(R.id.btn_social_group);
+        mSocialFocusBtn = findViewById(R.id.btn_social_focus);
+        mSocialCollectionBtn = findViewById(R.id.btn_social_collection);
+        mSocialMessageBtn = findViewById(R.id.btn_social_message);
+
+        mSocialGroupBtn.setOnClickListener(this);
+        mSocialFocusBtn.setOnClickListener(this);
+        mSocialCollectionBtn.setOnClickListener(this);
+        mSocialMessageBtn.setOnClickListener(this);
     }
 
     @Override
@@ -37,6 +58,11 @@ public class PersonSocialView extends LinearLayout implements IView {
 
     @Override
     public void onActive() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
