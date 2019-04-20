5fda28d678e7038e920230f48bd588f19a60f16e
-4 <2646094630@qq.com>
2019-04-20 22:47:44 +0800

    个人中心UI

--- app/src/main/java/com/rdc/project/traveltrace/view/custom_view/PersonSocialView.java
+++ app/src/main/java/com/rdc/project/traveltrace/view/custom_view/PersonSocialView.java
@@ -3,9 +3,13 @@ package com.rdc.project.traveltrace.view.custom_view;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.rdc.project.traveltrace.R;
 import com.rdc.project.traveltrace.arch.view.IView;
+import com.rdc.project.traveltrace.utils.DensityUtil;
 
 public class PersonSocialView extends LinearLayout implements IView {
 
@@ -19,6 +23,11 @@ public class PersonSocialView extends LinearLayout implements IView {
 
     public PersonSocialView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        LayoutInflater.from(context).inflate(R.layout.layout_person_social, this);
+        setOrientation(HORIZONTAL);
+        setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        int padding = DensityUtil.dp2px(10, context);
+        setPadding(padding, padding, padding, padding);
     }
 
     @Override
