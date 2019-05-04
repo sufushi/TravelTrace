dd66d649fca2f9a4292e557ff9248b83ee32396b
-4 <2646094630@qq.com>
2019-05-04 14:23:16 +0800

    PersonHeaderView数据层逻辑

--- app/src/main/java/com/rdc/project/traveltrace/view/custom_view/PersonHeaderView.java
+++ app/src/main/java/com/rdc/project/traveltrace/view/custom_view/PersonHeaderView.java
@@ -5,11 +5,20 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.meg7.widget.CircleImageView;
 import com.rdc.project.traveltrace.R;
+import com.rdc.project.traveltrace.entity.User;
 import com.rdc.project.traveltrace.utils.DensityUtil;
 
+import cn.bmob.v3.BmobUser;
+
 public class PersonHeaderView extends RelativeLayout {
+
+    private CircleImageView mUserIconView;
+    private TextView mUserNameView;
 
     public PersonHeaderView(Context context) {
         this(context, null);
@@ -24,6 +33,18 @@ public class PersonHeaderView extends RelativeLayout {
         LayoutInflater.from(context).inflate(R.layout.layout_person_header, this);
         int height = DensityUtil.dp2px(140, context);
         setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height));
+        mUserIconView = findViewById(R.id.user_icon);
+        mUserNameView = findViewById(R.id.user_name);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (BmobUser.isLogin()) {
+            User user = BmobUser.getCurrentUser(User.class);
+            Glide.with(getContext()).load(user.getUserIcon()).into(mUserIconView);
+            mUserNameView.setText(user.getUsername());
+        }
 
     }
 }
